bf48e12b8b7523899be7308aec3df9779a313ea5
junwoo45 <sinyoorang@gmail.com>
2020-05-29 16:01:07 +0900

    feat: 단어 submit할 때마다 unsplash API로 이미지 검색

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            term: '' 
+        };
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.runMeWhenUserSubmit(this.state.term);
+    }
+
+    onChangeValue = (e) => {
+        this.setState({ term: e.target.value });
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        {/* <input type="text" onChange={this.onInputChange}></input> */}
-                        <input type="text" onChange={(event) => console.log(event.target.value)}></input>
+                        <input type="text" onChange={this.onChangeValue} value={this.state.term}></input>
                     </div>
                 </form>
             </div>
