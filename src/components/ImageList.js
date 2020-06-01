3e2a8df786d9db57cb7da3eaf14018108ad9d50f
junwoo45 <sinyoorang@gmail.com>
2020-06-01 17:01:29 +0900

    feat: CSS 작업

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  return props.images.map(({ description, id, urls }) => {
-    return <img 
-      src = {urls.regular} 
-      key = {id}
-      alt = {description}
-    />
-  })
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image}></ImageCard>
+  });
+  
+  return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
